659bed6fdda381069dd2d3b036594ef6abfb4283
Yuichiro Nakada <berry@berry-lab.net>
2017-03-20 17:18:07 +0900

    added kbhit.h and ls.h

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 Single file libraries for C/C++.
 
-|library | lastest version | category | LoC | license | description
-| --------------------- | ---- | -------- | --- | --- | --------------------------------
-|**alsa.h** | 1.00 | audio | 121 | public domain | ALSA
-|**wav.h** | 1.00 | audio | 61 | public domain | Wave format
-|**v4l2.h** | 1.00 | video | 614 | public domain | video for linux
-|**jo_mpeg.h** | 1.00 | video | 317 | public domain | MPEG1/2 encoder [http://www.jonolick.com/home/mpeg-video-writer]
-|**minimp3.h** | 1.00 | audio | 2671 | LGPL | MP3 decoder [http://www.murga-linux.com/puppy/viewtopic.php?t=59417]
+|library | category | LoC | license | description
+| --------------------- | -------- | --- | --- | --------------------------------
+|**alsa.h** | audio | 121 | public domain | ALSA [sample: https://github.com/yui0/aplay-]
+|**wav.h** | audio | 61 | public domain | Wave format [sample: https://github.com/yui0/aplay-]
+|**v4l2.h** | video | 614 | public domain | video for linux
+|**kbhit.h** | utils | 60 | public domain | kbhit for linux [sample: https://github.com/yui0/aplay-]
+|**ls.h** | utils | 122 | public domain | making list of files and directories [sample: https://github.com/yui0/aplay-]
+|**jo_mpeg.h** | video | 317 | public domain | MPEG1/2 encoder [http://www.jonolick.com/home/mpeg-video-writer]
+|**minimp3.h** | audio | 2671 | LGPL | MP3 decoder [http://www.murga-linux.com/puppy/viewtopic.php?t=59417]
 
 ## References
 - [A single-header ANSI C gui library ](https://github.com/vurtun/nuklear)
---
+++ ls.h
@@ -0,0 +1,122 @@
+/* public domain Simple, Minimalistic, making list of files and directories
+ *	©2017 Yuichiro Nakada
+ *
+ * */
+
+#include <dirent.h>
+//#include <unistd.h>
+#include <sys/stat.h>
+
+typedef struct {
+	//int num;
+	int status;
+	char d_name[PATH_MAX];
+} LS_LIST;
+
+int count_dir(char *dir, int flag)
+{
+	DIR *dp;
+	struct dirent *entry;
+	struct stat statbuf;
+
+	if (!(dp = opendir(dir))) {
+		perror("opendir");
+		return 0;
+	}
+	char *cpath = get_current_dir_name();
+	chdir(dir);
+
+	int i=0;
+	while ((entry = readdir(dp))) {
+		if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name)) continue;
+
+		if (flag) {
+			stat(entry->d_name, &statbuf);
+			if (S_ISDIR(statbuf.st_mode)) {
+				char path[PATH_MAX];
+				sprintf(path, "%s/%s", dir, entry->d_name);
+				i += count_dir(path, flag);
+			}
+		}
+
+		i++;
+	}
+
+	closedir(dp);
+	chdir(cpath);
+	free(cpath);
+
+	return i;
+}
+	
+int seek_dir(char *dir, LS_LIST *ls, int flag)
+{
+	DIR *dp;
+	struct dirent *entry;
+	struct stat statbuf;
+	char buf[PATH_MAX];
+
+	if (!(dp = opendir(dir))) {
+		perror("opendir");
+		printf("%s\n", dir);
+		return 0;
+	}
+	char *cpath = get_current_dir_name();
+	chdir(dir);
+
+	int i=0;
+	while ((entry = readdir(dp))) {
+		if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name)) continue;
+
+		sprintf(buf, "%s/%s", dir, entry->d_name);
+		strcpy((ls+i)->d_name, buf);
+
+		stat(entry->d_name, &statbuf);
+		if (S_ISDIR(statbuf.st_mode)) {
+			//sprintf(buf, "%s/", entry->d_name);
+			//strcpy((ls+i)->d_name, buf);
+			(ls+i)->status = 1;
+
+			//if (flag) printf("+%s\n", buf);
+			if (flag) i += seek_dir(buf, ls+i+1, flag);
+		} else {
+			//strcpy((ls+i)->d_name, entry->d_name);
+			(ls+i)->status = 0;
+		}
+		//(ls+i)->num = i;
+		i++;
+	}
+
+	closedir(dp);
+	chdir(cpath);
+	free(cpath);
+
+	return i;
+}
+
+int comp_func(const void *a, const void *b)
+{
+	return (strcmp((char*)(((LS_LIST*)a)->d_name), (char*)(((LS_LIST*)b)->d_name)));
+}
+
+LS_LIST *ls_dir(char *dir, int flag, int *num)
+{
+	int n = count_dir(dir, flag);
+	if (!n) {
+		fprintf(stderr, "No file found [%s]!!\n", dir);
+		return 0;
+	}
+
+	LS_LIST *ls = (LS_LIST *)calloc(n, sizeof(LS_LIST));
+	if (!ls) {
+		perror("calloc");
+		return 0;
+	}
+
+	if (!seek_dir(dir, ls, flag)) return 0;
+
+	qsort(ls, n, sizeof(LS_LIST), comp_func);
+	*num = n;
+	return ls;
+}
+
