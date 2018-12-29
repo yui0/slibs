43fbe6fbedae95e8497c8a6a793cc8116b245a18
Yuichiro Nakada <berry@berry-lab.net>
2018-12-29 14:03:24 +0900

    improve the code

--- sgemm_sse.h
+++ sgemm_sse.h
@@ -131,11 +131,11 @@ static inline void dot8x8_avx(const float *a, const float *b, float *c, const in
 		b5 = _mm256_broadcast_ss(b+5);
 		b6 = _mm256_broadcast_ss(b+6);
 		b7 = _mm256_broadcast_ss(b+7);
-#ifdef CATSEYS_ALIGNED
+//#ifdef CATSEYE_ALIGNED
 		a0 = _mm256_load_ps(a);
-#else
-		a0 = _mm256_loadu_ps(a);
-#endif
+//#else
+//		a0 = _mm256_loadu_ps(a);
+//#endif
 
 		a += 8;
 		b += 8;
@@ -152,7 +152,7 @@ static inline void dot8x8_avx(const float *a, const float *b, float *c, const in
 		c7 = _mm256_add_ps(c7, _mm256_mul_ps(a0, b7));
 	}
 
-#ifdef CATSEYS_ALIGNED
+//#ifdef CATSEYE_ALIGNED
 	_mm256_store_ps(c, c0);
 	_mm256_store_ps(c+8, c1);
 	_mm256_store_ps(c+16, c2);
@@ -161,7 +161,7 @@ static inline void dot8x8_avx(const float *a, const float *b, float *c, const in
 	_mm256_store_ps(c+40, c5);
 	_mm256_store_ps(c+48, c6);
 	_mm256_store_ps(c+56, c7);
-#else
+/*#else
 	_mm256_storeu_ps(c, c0);
 	_mm256_storeu_ps(c+8, c1);
 	_mm256_storeu_ps(c+16, c2);
@@ -170,7 +170,7 @@ static inline void dot8x8_avx(const float *a, const float *b, float *c, const in
 	_mm256_storeu_ps(c+40, c5);
 	_mm256_storeu_ps(c+48, c6);
 	_mm256_storeu_ps(c+56, c7);
-#endif
+#endif*/
 }
 //  Micro kernel for multiplying panels from A and B.
 static inline void _sgemm_micro_kernel(
