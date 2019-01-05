77efdad4aa0db30d60c6768109af70f42a0eb95a
Yuichiro Nakada <berry@berry-lab.net>
2019-01-05 14:04:58 +0900

    add error messgaes

--- ocl.h
+++ ocl.h
@@ -21,6 +21,10 @@
 #define MAX_PLATFORMS	10
 #define MAX_DEVICES	10
 
+#define OCL_OUTPUT	1
+#define OCL_INPUT	2
+#define OCL_INPUT_ONCE	4
+// deprecated
 #define OCL_READ	1
 #define OCL_WRITE	2
 #define OCL_WRITE_ONCE	4
@@ -31,6 +35,74 @@ static void __checkOclErrors(const cl_int err, const char* const func, const cha
 {
 	if (err != CL_SUCCESS) {
 		fprintf(stderr, "OpenCL error at %s:%d code=%d \"%s\" \n", file, line, err, func);
+
+		switch (err) {
+		case CL_DEVICE_NOT_FOUND:                 printf("-- Error at %d:  Device not found.\n", line); break;
+		case CL_DEVICE_NOT_AVAILABLE:             printf("-- Error at %d:  Device not available\n", line); break;
+		case CL_COMPILER_NOT_AVAILABLE:           printf("-- Error at %d:  Compiler not available\n", line); break;
+		case CL_MEM_OBJECT_ALLOCATION_FAILURE:    printf("-- Error at %d:  Memory object allocation failure\n", line); break;
+		case CL_OUT_OF_RESOURCES:                 printf("-- Error at %d:  Out of resources\n", line); break;
+		case CL_OUT_OF_HOST_MEMORY:               printf("-- Error at %d:  Out of host memory\n", line); break;
+		case CL_PROFILING_INFO_NOT_AVAILABLE:     printf("-- Error at %d:  Profiling information not available\n", line); break;
+		case CL_MEM_COPY_OVERLAP:                 printf("-- Error at %d:  Memory copy overlap\n", line); break;
+		case CL_IMAGE_FORMAT_MISMATCH:            printf("-- Error at %d:  Image format mismatch\n", line); break;
+		case CL_IMAGE_FORMAT_NOT_SUPPORTED:       printf("-- Error at %d:  Image format not supported\n", line); break;
+		case CL_BUILD_PROGRAM_FAILURE:            printf("-- Error at %d:  Program build failure\n", line); break;
+		case CL_MAP_FAILURE:                      printf("-- Error at %d:  Map failure\n", line); break;
+		case CL_INVALID_VALUE:                    printf("-- Error at %d:  Invalid value\n", line); break;
+		case CL_INVALID_DEVICE_TYPE:              printf("-- Error at %d:  Invalid device type\n", line); break;
+		case CL_INVALID_PLATFORM:                 printf("-- Error at %d:  Invalid platform\n", line); break;
+		case CL_INVALID_DEVICE:                   printf("-- Error at %d:  Invalid device\n", line); break;
+		case CL_INVALID_CONTEXT:                  printf("-- Error at %d:  Invalid context\n", line); break;
+		case CL_INVALID_QUEUE_PROPERTIES:         printf("-- Error at %d:  Invalid queue properties\n", line); break;
+		case CL_INVALID_COMMAND_QUEUE:            printf("-- Error at %d:  Invalid command queue\n", line); break;
+		case CL_INVALID_HOST_PTR:                 printf("-- Error at %d:  Invalid host pointer\n", line); break;
+		case CL_INVALID_MEM_OBJECT:               printf("-- Error at %d:  Invalid memory object\n", line); break;
+		case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:  printf("-- Error at %d:  Invalid image format descriptor\n", line); break;
+		case CL_INVALID_IMAGE_SIZE:               printf("-- Error at %d:  Invalid image size\n", line); break;
+		case CL_INVALID_SAMPLER:                  printf("-- Error at %d:  Invalid sampler\n", line); break;
+		case CL_INVALID_BINARY:                   printf("-- Error at %d:  Invalid binary\n", line); break;
+		case CL_INVALID_BUILD_OPTIONS:            printf("-- Error at %d:  Invalid build options\n", line); break;
+		case CL_INVALID_PROGRAM:                  printf("-- Error at %d:  Invalid program\n", line); break;
+		case CL_INVALID_PROGRAM_EXECUTABLE:       printf("-- Error at %d:  Invalid program executable\n", line); break;
+		case CL_INVALID_KERNEL_NAME:              printf("-- Error at %d:  Invalid kernel name\n", line); break;
+		case CL_INVALID_KERNEL_DEFINITION:        printf("-- Error at %d:  Invalid kernel definition\n", line); break;
+		case CL_INVALID_KERNEL:                   printf("-- Error at %d:  Invalid kernel\n", line); break;
+		case CL_INVALID_ARG_INDEX:                printf("-- Error at %d:  Invalid argument index\n", line); break;
+		case CL_INVALID_ARG_VALUE:                printf("-- Error at %d:  Invalid argument value\n", line); break;
+		case CL_INVALID_ARG_SIZE:                 printf("-- Error at %d:  Invalid argument size\n", line); break;
+		case CL_INVALID_KERNEL_ARGS:              printf("-- Error at %d:  Invalid kernel arguments\n", line); break;
+		case CL_INVALID_WORK_DIMENSION:           printf("-- Error at %d:  Invalid work dimensionsension\n", line); break;
+		case CL_INVALID_WORK_GROUP_SIZE:          printf("-- Error at %d:  Invalid work group size\n", line); break;
+		case CL_INVALID_WORK_ITEM_SIZE:           printf("-- Error at %d:  Invalid work item size\n", line); break;
+		case CL_INVALID_GLOBAL_OFFSET:            printf("-- Error at %d:  Invalid global offset\n", line); break;
+		case CL_INVALID_EVENT_WAIT_LIST:          printf("-- Error at %d:  Invalid event wait list\n", line); break;
+		case CL_INVALID_EVENT:                    printf("-- Error at %d:  Invalid event\n", line); break;
+		case CL_INVALID_OPERATION:                printf("-- Error at %d:  Invalid operation\n", line); break;
+		case CL_INVALID_GL_OBJECT:                printf("-- Error at %d:  Invalid OpenGL object\n", line); break;
+		case CL_INVALID_BUFFER_SIZE:              printf("-- Error at %d:  Invalid buffer size\n", line); break;
+		case CL_INVALID_MIP_LEVEL:                printf("-- Error at %d:  Invalid mip-map level\n", line); break;
+		case -1024:                               printf("-- Error at %d:  *clBLAS* Functionality is not implemented\n", line); break;
+		case -1023:                               printf("-- Error at %d:  *clBLAS* Library is not initialized yet\n", line); break;
+		case -1022:                               printf("-- Error at %d:  *clBLAS* Matrix A is not a valid memory object\n", line); break;
+		case -1021:                               printf("-- Error at %d:  *clBLAS* Matrix B is not a valid memory object\n", line); break;
+		case -1020:                               printf("-- Error at %d:  *clBLAS* Matrix C is not a valid memory object\n", line); break;
+		case -1019:                               printf("-- Error at %d:  *clBLAS* Vector X is not a valid memory object\n", line); break;
+		case -1018:                               printf("-- Error at %d:  *clBLAS* Vector Y is not a valid memory object\n", line); break;
+		case -1017:                               printf("-- Error at %d:  *clBLAS* An input dimension (M,N,K) is invalid\n", line); break;
+		case -1016:                               printf("-- Error at %d:  *clBLAS* Leading dimension A must not be less than the size of the first dimension\n", line); break;
+		case -1015:                               printf("-- Error at %d:  *clBLAS* Leading dimension B must not be less than the size of the second dimension\n", line); break;
+		case -1014:                               printf("-- Error at %d:  *clBLAS* Leading dimension C must not be less than the size of the third dimension\n", line); break;
+		case -1013:                               printf("-- Error at %d:  *clBLAS* The increment for a vector X must not be 0\n", line); break;
+		case -1012:                               printf("-- Error at %d:  *clBLAS* The increment for a vector Y must not be 0\n", line); break;
+		case -1011:                               printf("-- Error at %d:  *clBLAS* The memory object for Matrix A is too small\n", line); break;
+		case -1010:                               printf("-- Error at %d:  *clBLAS* The memory object for Matrix B is too small\n", line); break;
+		case -1009:                               printf("-- Error at %d:  *clBLAS* The memory object for Matrix C is too small\n", line); break;
+		case -1008:                               printf("-- Error at %d:  *clBLAS* The memory object for Vector X is too small\n", line); break;
+		case -1007:                               printf("-- Error at %d:  *clBLAS* The memory object for Vector Y is too small\n", line); break;
+		case -1001:                               printf("-- Error at %d:  Code -1001: no GPU available?\n", line); break;
+		default:                                  printf("-- Error at %d:  Unknown with code %d\n", line, error);
+		}
 	}
 }
 #else
@@ -138,7 +210,7 @@ void oclKernel(ocl_t *kernel, int n, char *opt, char *kernel_code)
 		ret = clGetProgramBuildInfo(program, device_id[ocl_device], CL_PROGRAM_BUILD_LOG, 0, NULL, &len);
 		char *buffer = calloc(len, sizeof(char));
 		ret = clGetProgramBuildInfo(program, device_id[ocl_device], CL_PROGRAM_BUILD_LOG, len, buffer, NULL);
-		printf("\n%s", kernel_code);
+		printf("\n%s\n", kernel_code);
 		printf("\n%s\n", buffer);
 	}
 	for (int i=0; i<n; i++) {
@@ -173,9 +245,9 @@ void oclKernelArgs(ocl_t *kernel, int n)
 static inline void oclKernelArgsWrite(args_t *args)
 {
 	while (args->size) {
-		if (args->flag & OCL_WRITE) {
+		if (args->flag & OCL_INPUT) {
 			clEnqueueWriteBuffer(command_queue, args->p, CL_TRUE, 0, args->size, args->s, 0, 0, 0);
-			if (args->flag & OCL_WRITE_ONCE) args->flag ^= OCL_WRITE;
+			if (args->flag & OCL_INPUT_ONCE) args->flag ^= OCL_INPUT;
 //			printf("clEnqueueWriteBuffer size:%d %x\n", args->size, args->s);
 		}
 		args++;
@@ -185,7 +257,7 @@ static inline void oclKernelArgsWrite(args_t *args)
 static inline void oclKernelArgsRead(args_t *args)
 {
 	while (args->size) {
-		if (args->flag & OCL_READ) {
+		if (args->flag & OCL_OUTPUT) {
 			clEnqueueReadBuffer(command_queue, args->p, CL_TRUE, 0, args->size, args->s, 0, 0, 0);
 //			printf("clEnqueueReadBuffer size:%d %x\n", args->size, args->s);
 		}
